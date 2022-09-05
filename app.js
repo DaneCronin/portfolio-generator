775ff8ec478f7d1f322818c086500901facc6d5d
Dane Cronin <danecronin@gmail.com>
2022-09-04 21:49:43 -0600

    generated initial HTML File

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const [name, github] = profileDataArgs;
+
 //console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-}
 
-printProfileData(profileDataArgs);
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
+    
+    console.log('Portfolio Complete! Checkout index.html to see the output!');
+});
