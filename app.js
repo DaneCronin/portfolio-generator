ba1eaf127c5ca244ca9669162a576ea483970e98
Dane Cronin <danecronin@gmail.com>
2022-09-09 18:22:39 -0600

    added generateAbout function to add about section if there is one

--- app.js
+++ app.js
@@ -150,11 +150,11 @@ const promptUser = () => {
     .then(portfolioData => {
         const pageHTML = generatePage(portfolioData);
 
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
 
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
         
       });
 
